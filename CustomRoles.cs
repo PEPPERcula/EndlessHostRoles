3d65a5fe28ffa899d6ac0856d0e8137e9a45c223
PEPPERcula <matthew.d.schlecht@gmail.com>
2026-01-21 08:11:04 -0700

    Update CustomRoles.cs

--- CustomRoles.cs
+++ CustomRoles.cs
@@ -432,16 +432,16 @@ public enum CustomRoles
     // Solo PVP
     Challenger,
 
-    // FFA
+    // Free For All
     Killer,
 
-    // Move And Stop
+    // Stop And Go
     Tasker,
 
     // Hot Potato
     Potato,
 
-    // H&S
+    // Hide And Seek
     Hider,
     Seeker,
     Fox,
