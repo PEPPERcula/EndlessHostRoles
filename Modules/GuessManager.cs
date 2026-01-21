034b262bdcb4a0c03ec4f94d2044a63be616aff0
PEPPERcula <matthew.d.schlecht@gmail.com>
2026-01-21 08:02:04 -0700

    Update GuessManager.cs

--- Modules/GuessManager.cs
+++ Modules/GuessManager.cs
@@ -216,9 +216,7 @@ public static class GuessManager
 
                     switch (pc.GetCustomRole())
                     {
-                        case CustomRoles.Augur when !((Augur)Main.PlayerStates[pc.PlayerId].Role).CanGuess:
-                        case CustomRoles.Augur when Main.GuesserGuessed[pc.PlayerId] >= Augur.MaxGuessesPerGame.GetInt():
-                        case CustomRoles.Augur when Main.GuesserGuessedMeeting[pc.PlayerId] >= Augur.MaxGuessesPerMeeting.GetInt():
+                        case CustomRoles.Augur when !((Augur)Main.PlayerStates[pc.PlayerId].Role).CanGuess || Main.GuesserGuessed[pc.PlayerId] >= Augur.MaxGuessesPerGame.GetInt() || Main.GuesserGuessedMeeting[pc.PlayerId] >= Augur.MaxGuessesPerMeeting.GetInt():
                         case CustomRoles.NiceGuesser when Main.GuesserGuessed[pc.PlayerId] >= Options.GGCanGuessTime.GetInt():
                         case CustomRoles.EvilGuesser when Main.GuesserGuessed[pc.PlayerId] >= Options.EGCanGuessTime.GetInt():
                             ShowMessage("GGGuessMax");
