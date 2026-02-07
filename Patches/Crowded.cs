0a377e83c1f2f93bc60b420eea17dee01f3324ae
PEPPERcula <matthew.d.schlecht@gmail.com>
2026-02-06 20:02:07 -0700

    Update Crowded.cs

--- Patches/Crowded.cs
+++ Patches/Crowded.cs
@@ -401,13 +401,14 @@ public class AbstractPagingBehaviour(IntPtr ptr) : MonoBehaviour(ptr)
     public virtual void Update()
     {
         bool chatIsOpen = HudManager.Instance.Chat.IsOpenOrOpening;
+        bool gameMenuIsOpen = HudManager.Instance.GameMenu.IsOpen;
         
         if (Input.touchSupported)
         {
             foreach (Touch touch in Input.touches)
             {
                 if (touch.phase != TouchPhase.Moved) continue;
-                if (chatIsOpen) break;
+                if (chatIsOpen || gameMenuIsOpen) break;
 
                 if (touch.deltaPosition.y > 0f)
                 {
@@ -422,9 +423,9 @@ public class AbstractPagingBehaviour(IntPtr ptr) : MonoBehaviour(ptr)
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || (!chatIsOpen && Input.mouseScrollDelta.y > 0f))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || (!chatIsOpen || !gameMenuIsOpen && Input.mouseScrollDelta.y > 0f))
             Cycle(false);
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow) || (!chatIsOpen && Input.mouseScrollDelta.y < 0f))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow) || (!chatIsOpen || !gameMenuIsOpen && Input.mouseScrollDelta.y < 0f))
             Cycle(true);
     }
 
